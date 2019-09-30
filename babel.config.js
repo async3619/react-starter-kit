94b07d0fe974020b2539b5a6b31eb7788051fb85
Sophia <beyondthesophia@gmail.com>
2019-09-30 15:42:56 +0900

    update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -11,10 +11,6 @@ module.exports = {
         "@babel/preset-react",
         "@babel/preset-typescript",
     ],
-    plugins: [
-        ["@babel/plugin-proposal-decorators", { legacy: true }],
-        "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-syntax-dynamic-import",
-    ],
+    plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-syntax-dynamic-import"],
     ignore: ["node_modules", "build"],
 };
